2708518a575829e1bf15147ce955727af2dd72f3
EugeneBarD <55768419+EugeneBarD@users.noreply.github.com>
2020-10-03 23:26:00 +0300

    feat: add functions min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array === undefined || array.length === 0) return 0;
+	var minimum = array[0];
+	for (var i = 0; i < array.length; i++) {
+	minimum = minimum < array[i] ? minimum : array[i];
+	}
+  return minimum || 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array === undefined || array.length === 0) return 0;
+ 	var maximum = array[0];
+	for (var i = 0; i < array.length; i++) {
+	maximum = maximum > array[i] ? maximum : array[i];
+	}
+  return maximum || 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array === undefined || array.length === 0) return 0;
+	var sum = 0;
+	for (var i = 0; i < array.length; i++) {
+	sum += array[i];
+	}
+	var average = sum / (array.length);
+  return average || 0;
 }
